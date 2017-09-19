418d1b00f5e1187165b8d1ca734f584e6c2ee8c9
andrew <9509097@student.swin.edu.au>
2017-09-19 12:16:51 +1000

    Remove subdomain/hostname requirements

--- server.js
+++ server.js
@@ -289,14 +289,14 @@ module.exports = function(opt) {
 
     app.get('/:req_id', function(req, res, next) {
         const req_id = req.params.req_id;
-
+        /*
         // limit requested hostnames to 63 characters
         if (! /^[a-z0-9]{4,63}$/.test(req_id)) {
             const err = new Error('Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.');
             err.statusCode = 403;
             return next(err);
         }
-
+        */
         debug('making new client with id %s', req_id);
         new_client(req_id, opt, function(err, info) {
             if (err) {
